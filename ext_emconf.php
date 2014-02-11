e90bbeda13aca0ef88336dd7d935fee3cff88e7a
Helge Funk <helge.funk@e-net.info>
2014-02-11 13:48:22 +0100

    [TASK] Added initial composer.json and ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,43 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "migrate"
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Database migrations',
+	'description' => '',
+	'category' => 'e-net',
+	'author' => 'Helge Funk',
+	'author_email' => 'helge.funk@e-net.info',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
