4ab3787f4d29f40a1fa8324d39590fa2882dc3c8
Thomas Christiansen <thomas.christiansen@e-net.info>
2014-06-12 11:12:31 +0200

    [TASK] added database field script_path for backward compatibility

--- ext_tables.sql
+++ ext_tables.sql
@@ -11,6 +11,7 @@ CREATE TABLE tx_migrate_domain_model_migration (
 	driver varchar(255) DEFAULT '' NOT NULL,
 	version varchar(255) DEFAULT '' NOT NULL,
 	extension_key varchar(255) DEFAULT '' NOT NULL,
+	script_path varchar(255) DEFAULT '' NOT NULL,
 	extension_version varchar(255) DEFAULT '' NOT NULL,
 	identifier varchar(255) DEFAULT '' NOT NULL,
 	configuration text NOT NULL,
