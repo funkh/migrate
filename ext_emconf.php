ee7957b81b8d64f7a732aec33c0f5bcc5d0d10b3
Kemal Emre <k.emre@reply.de>
2015-10-30 17:04:35 +0100

    [TASK] Raised version to 1.2.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.1.1',
+	'version' => '1.2.1',
 	'constraints' => array(
 		'depends' => array(
 			'extbase' => '6.2',
@@ -41,4 +41,3 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 );
 
-?>
